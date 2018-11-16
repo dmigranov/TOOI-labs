07ef6fa5b7c6f8b2076e7551aa0380aca5f444be
Денис  Мигранов <d.migranov@g.nsu.ru>
2018-11-16 20:36:25 +0700

    lab6.1 done

--- Lab6/lab6.py
+++ Lab6/lab6.py
@@ -7,10 +7,46 @@ import xml.etree.ElementTree as etree
 tree = etree.parse('file.xml')
 root = tree.getroot()
 cnt = collections.Counter()
+bicnt = collections.Counter()
+order = collections.OrderedDict()
 
-for source in root.iter('source'):
-    l = source.text.split()
-    for word in l:
-        cnt[word] += 1 #word.lower()
 
-print(cnt.most_common(10))
+#for source in root.iter('source'):
+#    l = source.text.split()
+#    for word in l:
+#        cnt[word] += 1 #word.lower()
+
+prev = ""
+for token in root.iter('token'):
+    flag = True
+    biflag = True
+    word = token.get("text")
+    for g in token.iter('g'):
+        if g.get('v') == "CONJ" or g.get('v') == "PREP" or g.get('v') == "PNCT" or g.get('v') == "UNKN":
+            flag = False
+        if g.get('v') == "PNCT" or g.get('v') == "UNKN":
+            biflag = False
+        
+    if flag:
+        cnt[token.get("text")] += 1
+    if prev != "" and biflag:
+        bigram = (prev, word)
+        bicnt[bigram]+=1
+            
+    prev = word
+            
+
+lenorder = collections.OrderedDict(sorted(cnt.items(), key=lambda t: len(t[0]), reverse=True))
+unorder = collections.OrderedDict(sorted(cnt.items(), key=lambda t: t[1], reverse=False))
+unique =0
+for k,v in unorder.items():
+    if v == 1:
+        unique+=1
+
+print("#1")
+print("Most common", cnt.most_common(20))
+print("Least common", cnt.most_common()[-19:])
+print("Longest", list(lenorder.items())[0])
+print("Bigrams", bicnt.most_common(20))
+print("Unique", unique)
+print("Size", len(lenorder))
