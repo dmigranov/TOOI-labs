6c8c30b7025430e3e44cfa8a06be11f64581b68b
Денис  Мигранов <d.migranov@g.nsu.ru>
2018-11-23 19:35:59 +0700

    lab8 done!

--- Lab8/lab8.py
+++ Lab8/lab8.py
@@ -27,26 +27,38 @@ print(b1.b)
 #3
 
 class Word:
-    properties = []
-    def __init__(self, text):
+
+    def __init__(self, text, lemma):
+        self.properties = list()
         self.text = text
+        self.lemma = lemma
     def addProperty(self, p):
         self.properties.append(p)
         
 
 class Sentence:
-    sentence = []
 
+    def __init__(self):
+        self.words = list()
     def addWord(self, w):
-        self.sentence.append(w)
+        self.words.append(w)
+    def printSentence(self):
+        for word in words:
+            print (word.text)
         
 class Corpus:
-    corpus = []
 
+
+    def __init__(self):
+        self.sentences = list()
     def addSentence(self, s):
-        self.corpus.append(s)
+        self.sentences.append(s)
     def getWord(self, sentenceNumber, wordNumber):
-        return corpus.corpus[sentenceNumber].sentence[wordNumber].text
+        return self.sentences[sentenceNumber].words[wordNumber].text
+    def printWord(self, sentenceNumber, wordNumber):
+        print(self.getWord(sentenceNumber, wordNumber))
+        for p in self.sentences[sentenceNumber].words[wordNumber].properties:
+            print(p)
         
 
 import xml.etree.ElementTree as etree
@@ -54,16 +66,25 @@ tree = etree.parse('file.xml')
 root = tree.getroot()
 
 corpus = Corpus()
-for tokens in root.iter('tokens'):
+for sentence in root.iter('sentence'):
     s = Sentence()
+    #print('here')
+    tokens = sentence.find('tokens')
+
     for token in tokens:
-        w = Word(token.find('tfr').find('v').find('l').get('t'))
-        #print(token.find('tfr').find('v').find('l').get('t'))
-        for g in token.iter('g'):
+        w = Word(token.get('text'), token.find('tfr').find('v').find('l').get('t'))
+        for g in token.find('tfr').find('v').find('l').findall('g'):
             w.addProperty(g.get('v'))
+            #w.properties.append(g.get('v'))
         s.addWord(w)
+        #s.words.append(w)
+        
+    #s.printSentence()
     corpus.addSentence(s)
+    #corpus.sentences.append(s)
+
             
-print(corpus.getWord(0,1))
+#print(corpus.sentences[5].words[2].text)
+corpus.printWord(5,1)
 
 
