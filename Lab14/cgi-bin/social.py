efd7d4fba7e4edbc623dce15875e38577b963243
Денис  Мигранов <d.migranov@g.nsu.ru>
2018-11-30 22:21:33 +0700

    well, i guess it is done

--- Lab14/cgi-bin/social.py
+++ Lab14/cgi-bin/social.py
@@ -26,7 +26,7 @@ print('''
 </head>
 
 
-<body style = "background-color:silver">''')
+''')
 
 
 try:
@@ -34,36 +34,50 @@ try:
 		data = json.loads(file.read())
 		actualpassword = data['password']
 		if password == actualpassword:
-			string = 'Успех!'
+			message = form.getvalue('message')
+			if message != None:
+				data['messages'].append({"message" : message})
+				with open(filename, 'w') as file:
+					json.dump(data, file)
+			print('''
+			<body style = "background-color:silver">
+			<form action = "/cgi-bin/social.py" method = "post" >
+			<center>
+			<input type = "text" name = "message"> <br/>
+			<input type = "hidden" name = "login" value = %s>
+			<input type = "hidden" name = "password" value = %s>
+			<input type = "submit" value = "Запостить" />
+			</center>
+			</form>
+			<br>
+			<br>
+			''' % (login, password))
+			
+			
+			for message in data['messages']:
+				print('''
+				%s <br>
+				<br>
+				''' %(message['message']))
 		else:
-			string = 'Неправильный пароль!'
+			print('''
+			<body style = "background-color:silver">
+			<h1 align = "center" style = "color:steelblue">Неправильный пароль</h1> 
+			''')
 except:
-	string = 'Нет такого логина... Возможно, сначала надо зарегистрироваться?'
+
+	print('''
+	<body style = "background-color:silver">
+	<h1 align = "center" style = "color:steelblue">Нет такого логина... Возможно, сначала надо зарегистрироваться?</h1> 
+	''')
 
 print('''
-<!DOCTYPE html>
 
-<html>
-<head>
-<title>Не о такой победе над Майкрософтом я мечтал</title>
-<br>
-
-
-</head>
-
-
-<body style = "background-color:silver">
-	<h1 align = "center" style = "color:steelblue">%s</h1> 
 
 
 	
 
-
-
-
-
-
 </body>
 
 </html>
-''' % (string))
+''')
